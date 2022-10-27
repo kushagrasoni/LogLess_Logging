800becfcfe6466e2fd0a2c8732b5e2f9077ca857
simranBhamra <45669456+simranBhamra@users.noreply.github.com>
2022-10-27 18:51:55 -0400

    categorizer code

--- logless/categorizer.py
+++ logless/categorizer.py
@@ -1,4 +1,19 @@
 import sqlite3
+import ast
+from dataclasses import dataclass
+from xml.etree.ElementInclude import include
+
+
+@dataclass
+class CategoryAlias():
+    name :str
+    description : str
+    ast_node_class : str
+
+@dataclass
+class NodeWrapper():
+    ast : ast.AST
+    category_alias : CategoryAlias
 
 
 def get_category(ast_node):
@@ -13,9 +28,26 @@ def get_category(ast_node):
     # Algorithm for finding different possible patterns within a serverless app code
     # This will need extensive pattern based algorithms to identify relevant code which
     # should be logged
+    node_type_name = type(ast_node).__name__
 
     connection = sqlite3.connect('../db/logless.db')
     cursor = connection.cursor()
-    rows = cursor.execute("SELECT * FROM category").fetchall()
-    print(rows)
-    return rows
+    row = cursor.execute(f"SELECT * FROM category where name = '{node_type_name}' limit 1;").fetchone()
+    if row:
+        category_alias = CategoryAlias(*row)
+    else:
+        category_alias = CategoryAlias(None, None, None)
+    return NodeWrapper(ast_node, category_alias)
+        
+
+if __name__ == '__main__':
+    # would be handled by other functions
+    with open('../app/lambda_function.py', 'r') as f:
+        parsed_funct = ast.parse(f.read())
+    i = 0
+    for node in ast.walk(parsed_funct):
+        node_wrapper = get_category(node)
+        # print(f'For node {ast.dump(node_wrapper.ast)}, found category mapping {node_wrapper.category_alias}')
+        if node_wrapper.category_alias.name:
+            print(f'For node {i}, found category mapping {node_wrapper.category_alias}')
+            i += 1
