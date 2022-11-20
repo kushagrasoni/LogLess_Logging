2ee86aa39208d8e7b6d360bc129afa2e30d0034f
Kushagra Soni <soni.kushagra24@gmail.com>
2022-11-20 00:22:34 -0500

    Connected decorator.py with generator.py

--- logless/decorator.py
+++ logless/decorator.py
@@ -1,17 +1,22 @@
 import collections
 
 from sys import settrace
-from fpdf import FPDF
 
+# from fpdf import FPDF
+from logless.generator import LogEvent, Generator
 
 final_result = collections.OrderedDict()
 frame_to_local_reprs = {}
 func_name = None
 
-#event
-#event 
+# log_event = LogEvent()
+generator = Generator()
+
+
+# event
+# event
 # event type 
-#colour
+# colour
 # text
 
 def log(func):
@@ -28,7 +33,7 @@ def log(func):
     :param func: The input is the function to which decorator is used upon
     :return: The output of the function (if any)
     """
-    global func_name
+    global func_name, log_event
     func_name = func.__name__
 
     def getcode(*args, **kwargs):
@@ -59,92 +64,45 @@ def get_local_reprs(frame, watch=(), custom_repr=(), max_length=None, normalize=
 # local trace function which returns itself
 def my_tracer(frame, event, arg=None):
     function_name = frame.f_code.co_name
-    global final_result, frame_to_local_reprs
+    global final_result, frame_to_local_reprs, log_event
     if function_name == func_name:
         old_local_reprs = frame_to_local_reprs.get(frame, {})
         frame_to_local_reprs[frame] = final_result = get_local_reprs(frame)
 
-        newish_string = ('Starting var:.. ' if event == 'call' else
-                         'New var:....... ')
-        for name, value_repr in final_result.items():
-            if name not in old_local_reprs:
-                log_event = LogEvent(event,f'{event}->{newish_string}{name} = {value_repr}')
-                print(log_event)
-            elif old_local_reprs[name] != value_repr:
-                print(f'{event}->Modified var:.. {newish_string}{name} = {value_repr}')
+        assign_type = ('Starting var:.. ' if event == 'call' else
+                       'New var:....... ')
+        for var_name, var_value in final_result.items():
+            if var_name not in old_local_reprs:
+                # log_event.generate(event, f'{event}->{newish_string}{var_name} = {var_value}')
+                # log_event = LogEvent(event,f'{event}->{newish_string}{var_name} = {var_value}')
+                # print(f'{event}->{assign_type}{var_name} = {var_value}')
+                generator.log(event, assign_type, var_name, var_value)
+
+            elif old_local_reprs[var_name] != var_value:
+                assign_type = 'Modified var:..'
+                # print(f'{event}->Modified var:.. {assign_type}{var_name} = {var_value}')
+                generator.log(event, assign_type, var_name, var_value)
         if event == 'return':
             frame_to_local_reprs.pop(frame, None)
-            return_value_repr = arg
-            print(f'{event}->Return value:.. {return_value_repr}')
+            return_value = arg
+            assign_type = 'Return value:..'
+            var_name = 'return'
+            # print(f'{event}-> {assign_type} {return_value}')
+            generator.log(event, assign_type, var_name, return_value)
         # print(SetColor(event,'blue'))
         return my_tracer
     return None
 
-
-    # Event 
+    # Event
     # - name 
     # - type 
     # - text 
     # - colour 
 
 
-class LogEvent():
-    def __init__(self, event_type, event_text):
-        self.event_type = event_type
-        self.event_text = event_text
-        self.type_color_map = {
-            'line':'white',
-            'call':'blue'
-        }
-        self.color_map = {'black' : "\u001b[30m",
-                    'red' : "\u001b[31m",
-                    'green' : "\u001b[32m",
-                    'yellow' : "\u001b[33m",
-                    'blue' : "\u001b[34m",
-                    'magenta' : "\u001b[35m",
-                    'cyan' : "\u001b[36m",
-                    'white' : "\u001b[37m",
-                    'none':''
-        }
-
-    def color_code(self):
-        return self.color_map[self.type_color_map[self.event_type]]
-    
-    def __str__(self):
-        return f'{self.color_code()}{self.event_text}'
-
-
-
-class  SetColor():
-    def __init__(self, text, color  = 'none'):
-        self.text = text
-        self.color = color
-        self.color_map = {'black' : "\u001b[30m",
-                    'red' : "\u001b[31m",
-                    'green' : "\u001b[32m",
-                    'yellow' : "\u001b[33m",
-                    'blue' : "\u001b[34m",
-                    'magenta' : "\u001b[35m",
-                    'cyan' : "\u001b[36m",
-                    'white' : "\u001b[37m",
-                    'none':''
-    }
-
-    def color_code(self):
-        return self.color_map[self.color]
-    
-    def __str__(self):
-        return f'{self.color_code()}{self.text}'
-
-
-    
-
-
 def pdf_print():
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size = 20)
-    pdf.cell(200, 10, txt = "LogLess",ln = 1, align = 'C')
-    pdf.output("logless.pdf")  
-
-
+    pdf.set_font("Arial", size=20)
+    pdf.cell(200, 10, txt="LogLess", ln=1, align='C')
+    pdf.output("logless.pdf")
--- logless/generator.py
+++ logless/generator.py
@@ -1,42 +1,94 @@
-from logger import logger
+from logless.logger import logger
 import os
 import secrets
 
 from conf.config import MODE_CONFIG, LOG_CONFIG, INFO
 
 
+class LogEvent:
+    def __init__(self):
+        self.event_type = None
+        self.event_text = None
+        self.type_color_map = {
+            'line': 'white',
+            'call': 'blue'
+        }
+        self.color_map = {'black': "\u001b[30m",
+                          'red': "\u001b[31m",
+                          'green': "\u001b[32m",
+                          'yellow': "\u001b[33m",
+                          'blue': "\u001b[34m",
+                          'magenta': "\u001b[35m",
+                          'cyan': "\u001b[36m",
+                          'white': "\u001b[37m",
+                          'none': ''
+                          }
+
+    def generate(self, event_type, event_text):
+        self.event_type = event_type
+        self.event_text = event_text
+
+    def color_code(self):
+        return self.color_map[self.type_color_map[self.event_type]]
+
+    def __str__(self):
+        return f'{self.color_code()}{self.event_text}'
+
+
+class SetColor():
+    def __init__(self, text, color='none'):
+        self.text = text
+        self.color = color
+        self.color_map = {'black': "\u001b[30m",
+                          'red': "\u001b[31m",
+                          'green': "\u001b[32m",
+                          'yellow': "\u001b[33m",
+                          'blue': "\u001b[34m",
+                          'magenta': "\u001b[35m",
+                          'cyan': "\u001b[36m",
+                          'white': "\u001b[37m",
+                          'none': ''
+                          }
+
+    def color_code(self):
+        return self.color_map[self.color]
+
+    def __str__(self):
+        return f'{self.color_code()}{self.text}'
+
+
 class Generator:
-    def __init__(self, config, patterns, vars):
+    def __init__(self):
         """
         Constructor method
         """
         self.logger = logger
-        self.config = config
         self.mode_config = self.get_mode_config()
-        self.patterns = patterns
-        self.vars = vars
 
-    def log(self):
+
+    def log(self, event, assign_type, var_name, var_value):
         """
         Begins logging procedure. It iterates through each pattern and generates an appropriate logging message
         according to the characteristics. The logging messages then gets logged.
         """
-        for pattern in self.patterns:
-            if pattern.get("verbosity") == INFO and INFO in self.mode_config.get("SUPPORTED_LOG_LEVELS"):
-                self.log_info(pattern.get("pattern"))
+        # for pattern in self.patterns:
+        #     if pattern.get("verbosity") == INFO and INFO in self.mode_config.get("SUPPORTED_LOG_LEVELS"):
+        #         self.log_info(pattern.get("pattern"))
+        self.log_info(event, assign_type, var_name, var_value)
 
-    def log_info(self, pattern):
+    def log_info(self, event, assign_type, var_name, var_value):
         # Extract attributes from pattern
-        for spec in pattern:
-            if isinstance(spec, tuple):
-                msg = ""
-                for i in range(len(spec)):
-                    # TODO: determine if value should be logged according to configuration {LOG_VALUES}
-                    if i == len(spec) - 1:
-                        msg += f"{spec[i]}"
-                        break
-                    msg += f"{spec[i]}, "
-                self.logger.info(msg)
+        # for spec in pattern:
+        #     if isinstance(spec, tuple):
+        #         msg = ""
+        #         for i in range(len(spec)):
+        #             # TODO: determine if value should be logged according to configuration {LOG_VALUES}
+        #             if i == len(spec) - 1:
+        #                 msg += f"{spec[i]}"
+        #                 break
+        #             msg += f"{spec[i]}, "
+        #         self.logger.info(msg)
+        self.logger.info(f'{event}, {assign_type}, {var_name}, {var_value}')
 
     def log_error(self, pattern):
         # TODO
@@ -46,6 +98,7 @@ class Generator:
     # Add remaining verbosity methods
 
     """ STATIC METHODS """
+
     @staticmethod
     def get_mode_config():
         """
@@ -60,25 +113,25 @@ class Generator:
         return mode_config
 
 
-if __name__ == "__main__":
-    # example usage
-    patterns = [{
-        "pattern": [('Assign',
-                     'Total Targets: 1',
-                     'Targets: [<ast.Name object at 0x110d46d60>]',
-                     'Value: <ast.Call object at 0x110d46d30>'),
-                    ('Name', 'action_event', '<ast.Store object at 0x1104faa90>'),
-                    ('Call',
-                     '<ast.Attribute object at 0x110d46d00>',
-                     1,
-                     '[<ast.Constant object at 0x110d46c70>]'),
-                    ('Attribute',
-                     'Value: <ast.Name object at 0x110d46cd0>',
-                     'Attribute: get',
-                     'Context: <ast.Load object at 0x1104faa30>'),
-                    ('Name', 'event', '<ast.Load object at 0x1104faa30>'),
-                    ('Constant', 'action')],
-        "verbosity": "INFO"
-    }]
-    gen = Generator(LOG_CONFIG, patterns, None)
-    gen.log()
+# if __name__ == "__main__":
+#     # example usage
+#     patterns = [{
+#         "pattern": [('Assign',
+#                      'Total Targets: 1',
+#                      'Targets: [<ast.Name object at 0x110d46d60>]',
+#                      'Value: <ast.Call object at 0x110d46d30>'),
+#                     ('Name', 'action_event', '<ast.Store object at 0x1104faa90>'),
+#                     ('Call',
+#                      '<ast.Attribute object at 0x110d46d00>',
+#                      1,
+#                      '[<ast.Constant object at 0x110d46c70>]'),
+#                     ('Attribute',
+#                      'Value: <ast.Name object at 0x110d46cd0>',
+#                      'Attribute: get',
+#                      'Context: <ast.Load object at 0x1104faa30>'),
+#                     ('Name', 'event', '<ast.Load object at 0x1104faa30>'),
+#                     ('Constant', 'action')],
+#         "verbosity": "INFO"
+#     }]
+#     gen = Generator(LOG_CONFIG, patterns, None)
+#     gen.log()
