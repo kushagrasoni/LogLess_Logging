ee7f1dd59646db0304a032f3ebc748b2f8bbf5bd
Kushagra Soni <soni.kushagra24@gmail.com>
2022-11-20 23:33:32 -0500

    Removed Trash directory

--- trash/create.py
+++
@@ -1,9 +0,0 @@
-def get_log(pattern):
-    """
-    This Function create the log statement based on the pattern passed to it.
-
-    <Deails TBU>
-    :param pattern: Pattern input from the logless.decorator module
-    :return: Logging statement string
-    """
-    return f'Logging Statement Based on the \"{pattern}\" Pattern'
