367e192c7f7721d8fe49cd071026c7a24bc7f3d9
abshinde221 <ams9mb@virginia.edu>
2022-11-30 23:39:16 -0500

    more tests

--- tests/unit/test_generator.py
+++ tests/unit/test_generator.py
@@ -1,6 +1,6 @@
 import os
 
-from conf.config import MODE_CONFIG
+from conf.config import MODE_CONFIG, INFO
 
 
 class TestGenerator:
@@ -8,10 +8,17 @@ class TestGenerator:
     Testing suite for the Generator class
     """
 
+    event_type = "test event type"
+    assign_type = "test assign type"
+    var_name = "test var name"
+    var_value = "test var value"
+    level = INFO
+
     def test_get_mode_config_not_set(self, generator):
         """
         Tests getting logging mode configurations without prior setting
         """
+        os.environ["LOGGING_MODE"] = ""
         expected_mode_config = MODE_CONFIG.get("SAFE")
         actual_mode_config = generator.get_mode_config()
         assert expected_mode_config == actual_mode_config
@@ -25,3 +32,18 @@ class TestGenerator:
         actual_mode_config = generator.get_mode_config()
         assert expected_mode_config == actual_mode_config
 
+    def test_log(self, generator, mocker):
+        mock_log_info = mocker.patch.object(generator, "log_info")
+        generator.log(self.event_type, self.assign_type, self.var_name, self.var_value, self.level)
+        mock_log_info.assert_called_once_with(self.event_type, self.assign_type, self.var_name, self.var_value)
+
+    def test_log_unsupported(self, generator, mocker):
+        mock_log_info = mocker.patch.object(generator, "log_info")
+        generator.log(self.event_type, self.assign_type, self.var_name, self.var_value, "UNSUPPORTED")
+        mock_log_info.assert_not_called()
+
+    def test_log_info_with_values(self, generator, mocker):
+        mock_logger = mocker.patch.object(generator, "logger")
+        generator.log_info(self.event_type, self.assign_type, self.var_name, self.var_value)
+        mock_logger.info.assert_called_once_with(f"{self.event_type}, {self.assign_type}, {self.var_name}, "
+                                                 f"{self.var_value}")
