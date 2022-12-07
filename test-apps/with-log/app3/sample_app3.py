bac7958dbc4c316f15fb08233ced913562255332
simranBhamra <45669456+simranBhamra@users.noreply.github.com>
2022-12-06 21:47:26 -0500

    adding benchmarking files

---
+++ test-apps/with-log/app3/sample_app3.py
@@ -0,0 +1,13 @@
+import sys
+
+import requests
+
+sys.path.append('../../..')
+import logless
+
+
+@logless.log()
+def lambda_function(event, context):
+    api_url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.post(api_url, json=event)
+    return response.json()
